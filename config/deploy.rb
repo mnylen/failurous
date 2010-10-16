86e10ef92237e1fdb965ba501b413023436cb1b8
Mikko Nylén <mikko.nylen@gmail.com>
2010-10-16 04:47:34 +0300

    Dirty fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,9 +22,8 @@ end
 
 namespace :bundler do
   task :create_symlink, :roles => :app do
-    shared_dir = File.join(shared_path, 'bundle')
-    release_dir = File.join(current_release, '.bundle')
-    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+    bundled_gems_dir = File.join(shared_path, '.bundle')
+    run("mkdir -p #{bundled_gems_dir} && cd #{release_dir} && ln -s #{bundled_gems_dir} .")
   end
   
   task :bundle_new_release, :roles => :app do
