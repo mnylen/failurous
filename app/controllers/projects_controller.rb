84cebb8a8a5c567b7080b25f0ce2a38534ed2617
Mikko Nylén <mikko.nylen@eficode.fi>
2010-10-16 14:38:07 +0300

    Show all projects at left sidebar

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,13 @@
 class ProjectsController < ApplicationController
-  
+  before_filter :load_all_projects
+    
   def show
     @project = Project.first
   end
-
+  
+  private
+  
+    def load_all_projects
+      @projects = Project.all
+    end
 end
