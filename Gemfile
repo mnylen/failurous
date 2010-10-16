4ffd23b982a8190b23052e7ce927372ae7e63f61
Mikko Nylén <mikko.nylen@gmail.com>
2010-10-16 04:26:42 +0300

    Added Capistrano

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ end
 
 group :development do
   gem "thin"
+  gem "capistrano"
 end
 
 group :test do
---
+++ config/deploy.rb
@@ -0,0 +1,47 @@
+set :application, "failurous"
+set :repository,  "git@github.com:railsrumble/rr10-team-256.git"
+
+set :scm, :git
+
+role :web, :app, :db, "li220-76.members.linode.com"
+set :user, "www-data"
+set :password, "y34ZjdlAPf7"
+set :use_sudo, false
+set :deploy_to, "/var/rails/#{application}"
+set :rails_env, "production"
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(current_release, '.bundle')
+    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+  end
+  
+  task :bundle_new_release, :roles => :app do
+    bundler.create_symlink
+    run "cd #{release_path} && bundle install" # --without test"
+  end
+  
+  task :lock, :roles => :app do
+    run "cd #{current_release} && bundle lock;"
+  end
+  
+  task :unlock, :roles => :app do
+    run "cd #{current_release} && bundle unlock;"
+  end
+end
+
+after "deploy:update_code" do
+  bundler.create_symlink
+  bundler.bundle_new_release
+end
