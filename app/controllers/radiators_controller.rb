a6c0b195b9a728a90c2abd71611282042703ab20
Tero Parviainen <tero.parviainen@eficode.fi>
2010-10-16 18:05:48 +0300

    added top navigation + a placeholder for the radiator

---
+++ app/controllers/radiators_controller.rb
@@ -0,0 +1,6 @@
+class RadiatorsController < ApplicationController
+  
+  def show
+  end
+  
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  
+  def in_radiator?
+    controller_name == "radiators"
+  end
+  
 end
