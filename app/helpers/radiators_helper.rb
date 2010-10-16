60b5143c825a3519bef2824684168c12b5778b0b
Tero Parviainen <tero.parviainen@eficode.fi>
2010-10-16 20:46:32 +0300

    adjust radiator project sizes slightly depending on the number of projects

--- app/helpers/radiators_helper.rb
+++ app/helpers/radiators_helper.rb
@@ -1,2 +1,19 @@
 module RadiatorsHelper
+  
+  def ok_project_colspan
+    case @ok_projects.size
+    when 1..2 then 1
+    when 3..4 then 2
+    else 3
+    end
+  end
+  
+  def ok_project_size
+    case ok_project_colspan
+    when 1 then "large"
+    when 2 then "med"
+    else "small"
+    end
+  end
+  
 end
