fa3214f2e9cd27d9d9c32ba1d2e7348b63d9afe7
Tero Parviainen <tero.parviainen@eficode.fi>
2010-10-17 11:57:41 +0300

    shatner.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,10 @@
 class HomeController < ApplicationController
   
+  def index
+    unless cookies[:shatner]
+      @shatner = true
+      cookies[:shatner] = { :value => "tech wars", :expires => 1.year.from_now }
+    end
+  end
+  
 end
