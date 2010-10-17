3584e7174fc87de0abe2e57e5493ff34fd25bd63
Tero Parviainen <tero.parviainen@eficode.fi>
2010-10-18 00:53:11 +0300

    add fail gen to db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
+require "demo_fail_generator"
+
 Project.create(:name => "Shatner''s Bassoon Inc.")
 Project.create(:name => "Tech Wars")
 Project.create(:name => "Llama Inventory")
 Project.create(:name => "Sockswapping Intl.")
 Project.create(:name => "Fawlty Towers")
 
+DemoFailGenerator.new.run
 
