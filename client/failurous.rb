a4abbbd3c0c68bccc20f5d75108af9c6d0f8dea3
Mikko Nylén <mikko.nylen@gmail.com>
2010-10-19 17:15:01 +0300

    Moved the Rails client code to another repository (http://github.com/mnylen/failurous-rails)

--- client/failurous.rb
+++
@@ -1,7 +0,0 @@
-module Failurous
-  ROOT_PATH = File.dirname(__FILE__)
-  autoload :Config,            "#{ROOT_PATH}/failurous/config"
-  autoload :FailNotification,  "#{ROOT_PATH}/failurous/fail_notification"
-  autoload :FailNotifier,      "#{ROOT_PATH}/failurous/fail_notifier"
-  autoload :FailMiddleware,    "#{ROOT_PATH}/failurous/fail_middleware"
-end
