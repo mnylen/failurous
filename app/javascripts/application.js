1f13101a48deb2efd73a0bb756550b0d257d789b
Mikko Nylén <mikko.nylen@gmail.com>
2010-10-22 21:08:04 +0300

    Removed more deprecated JavaScript

--- app/javascripts/application.js
+++ app/javascripts/application.js
@@ -2,15 +2,6 @@
 //= require <jquery-ui-1.8.5.custom.min>
 
 jQuery(document).ready(function($) {
-
-    $("[data-fade]").each(function() {
-       var el = $(this);
-       setTimeout(function() {
-         el.remove();           
-       }, el.attr('data-fade') * 1000);
-    });
-    
-   
-    radiator.animateFails();
-    radiator.updatePeriodically();
+  radiator.animateFails();
+  radiator.updatePeriodically();
 });
