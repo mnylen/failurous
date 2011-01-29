34fad34bce63da3c30fa93e79f8f8eeed1604b73
Mikko Nylén <mikko.nylen@gmail.com>
2011-01-29 21:01:36 +0200

    Removed render_instructions helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,6 @@
 
 module ApplicationHelper
   
-  def render_instructions(project, name)
-    file = File.join(RAILS_ROOT, 'doc', 'clients', "#{name}.md")
-    content = File.read(file)
-    content.gsub!("<FAILUROUS-INSTALLATION>", root_url)
-    content.gsub!("<API-KEY-FOR-PROJECT>", project.api_key)
-    RDiscount.new(content, :smart).to_html.html_safe
-  end
-
   def positive_button(title, path, options = {})
     link_to(title, path, options.merge({:class => 'button positive'}))
   end
